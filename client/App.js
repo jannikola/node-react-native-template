9169fc280b479b458ece6eee37af183544fb88ed
jannikola <jannikola@gmail.com>
2020-10-24 22:47:10 +0200

    fetch & display test data

--- client/App.js
+++ client/App.js
@@ -1,12 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [data, setData] = useState("");
+
+  function getData() {
+    return fetch("http://localhost:3000/v1/initial", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((responseJson) => {
+        setData(responseJson);
+        return responseJson.club;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{data.club}</Text>
+      <Text>{data.league}</Text>
+      <Text>{data.website}</Text>
+      <Text>{data.position}</Text>
+      <Text>{data.stadium}</Text>
+      <Button title="Get Data" onPress={getData} />
     </View>
   );
 }
@@ -14,8 +34,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
