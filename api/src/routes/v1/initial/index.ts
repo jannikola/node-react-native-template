4dc799e1e8069269397b4258c0b43da573b8b291
jannikola <jannikola@gmail.com>
2020-10-24 14:29:09 +0200

    mocked data added

--- api/src/routes/v1/initial/index.ts
+++ api/src/routes/v1/initial/index.ts
@@ -1,11 +1,20 @@
 import { Router } from "express";
+
+const mockedData = {
+  club: "Liverpool",
+  league: "Premier League / England",
+  website: "https://www.liverpoolfc.com/",
+  position: "1st",
+  stadium: "Anfield Road",
+};
+
 export class InitialRoutes {
   private router: Router = Router();
 
   public getRouter(): Router {
     this.router.get("/initial", async (req, res) => {
       try {
-        res.send("The sedulous hyena ate the antelope!");
+        res.send(mockedData);
       } catch (e) {
         res.send({ e });
       }
